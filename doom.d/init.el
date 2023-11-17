0011d85854efb66e884231d96da622591322c554
Keenan Sanchez <keenan@gainful.com>
2023-11-17 00:22:40 -0500

    tabs

--- doom.d/init.el
+++ doom.d/init.el
@@ -30,7 +30,7 @@
        neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +all)   ; tame sudden yet inevitable temporary windows
-       ;; tabs              ; a tab bar for Emacs
+       tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
