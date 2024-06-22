411e001f9771c0f5d7c27b06fe91493884a21dd5
Keenan Sanchez <keejsan@gmail.com>
2024-06-21 22:01:56 -0400

    doom.d

--- doom.d/functions.el
+++ doom.d/functions.el
@@ -1,1 +1,16 @@
-;;; ../dotfiles/doom.d/functions.el -*- lexical-binding: t; -*-
+
+
+
+(defun ++workspace/switch-to-by-name (name &optional create)
+  (let ((index (cl-position name (+workspace-list-names) :test 'string=)))
+    (+workspace/switch-to index)))
+
+
+(defun ++workspace/workspace-p (name)
+  (not (null (-contains? (+workspace-list-names) name))))
+
+;; (++workspace/workspace-p "vterm")
+
+;; (++workspace/switch-to-by-name "vterm")
+;; (-elem-index "vterm" (+workspace-list-names))
+;; (-elem-index "main" (+workspace-list-names))
