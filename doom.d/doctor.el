4b8fe7296f2451e2fdda01982c16a3cf8a7174c1
Keenan Sanchez <keejsan@gmail.com>
2024-07-11 22:30:46 -0400

    doctor.el

--- doom.d/doctor.el
+++ doom.d/doctor.el
@@ -1,6 +1,9 @@
 ;;; ../codez/dotfiles/doom.d/doctor.el -*- lexical-binding: t; -*-
 
+(if (modulep! :lang python +conda)
+    (unless (executable-find "conda")
+      (warn! "conda is not installed.")))
 
 ;; TODO: this is giving a false positive.
-(unless (find-font (font-spec :family "Fira Code"))
-  (warn! "fira code font not found."))
+;; (unless (find-font (font-spec :family "Fira Code"))
+;;   (warn! "fira code font not found."))
