0d559c7658056eded6c6f02373affa493a88a7b3
Keenan Sanchez <keejsan@gmail.com>
2026-01-30 09:43:36 -0500

    x

--- doom.d/experimentals.el
+++ doom.d/experimentals.el
@@ -6,37 +6,22 @@
 (require 'evil-textobj-tree-sitter)
 (require 'typescript-ts-mode)
 
+treesit-explore-mode
 
+(treesit-language-available-p 'tsx)
 ;; working!
 (progn
   (setq query '((jsx_element) @jsx-tag))
+  (setq query '((jsx_element
+                 open_tag: (jsx_opening_element name: (identifier) @open-identifier) 
+                 close_tag: (jsx_closing_element name: (identifier) @close-identifier))))
+  
   (treesit-query-validate 'tsx query)
   (define-key evil-inner-text-objects-map "x" 
               (evil-textobj-tree-sitter-get-textobj
-                "jsx-tag"
-                '((tsx-ts-mode . ((jsx_element) @jsx-tag))
-                  ))
-              )
-  )
-
-;; (map! :map +tree-sitter-outer-text-objects-map
-;;       "m" (evil-textobj-tree-sitter-get-textobj "import"
-;;             '((python-mode . [(import_statement) @import])
-;;               (rust-mode . [(use_declaration) @import]))))
-
-
-
-(progn
-  (setq query
-        '((jsx_element
-           open_tag: (jsx_opening_element) @start
-           close_tag: (jsx_closing_element) @end)))
-  (define-key evil-inner-text-objects-map "x"
-              (evil-textobj-tree-sitter-get-textobj
-                "jsx-element"
-                query))
-
-  )
+                "close-identifier"
+                `((tsx-ts-mode . ,query)))
+              (setq foo 'bar)))
 
 (evil-define-operator evil-change--jsx-tag
   (orig-fn beg end type register yank-handler delete-func)
@@ -45,9 +30,7 @@
   (message "evil-change called with args: %s" (list orig-fn beg end type register yank-handler delete-func))
   (if nil
       nil
-    (apply orig-fn (list beg end type register yank-handler delete-func))
-    )
-  )
+    (apply orig-fn (list beg end type register yank-handler delete-func))))
 
 
 
@@ -65,23 +48,23 @@
       
 
       (defun fn (opening-tag-name)
-        (treesit-node-eq opening-tag-name (treesit-node-at (point)))
-        )
+        (treesit-node-eq opening-tag-name (treesit-node-at (point))))
+      
       
       (let* (
              (query '((jsx_element
                        open_tag: (jsx_opening_element name: (identifier) @opening-tag-name)
                        close_tag: (jsx_closing_element name: (identifier) @closing-tag-name)
-                       (:pred fn @opening-tag-name)
-                       )))
+                       (:pred fn @opening-tag-name))))
+             
              (_ (treesit-query-validate 'tsx query))
              ;; (captures '()))
              (captures (treesit-query-capture (treesit-buffer-root-node) query)))
         (cl-loop for (key . node) in captures
                  do (message "%s: '%s'" key (treesit-node-text node))
                  when nil
-                 return (treesit-node-text node))
-        )
-      )
+                 return (treesit-node-text node))))
+        
+      
   (other-window -1))
 
