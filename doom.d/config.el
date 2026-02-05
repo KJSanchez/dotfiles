3e911bf5462320cff44f5b9605ecbce03da3eab7
Keenan Sanchez <kesanchez@khov.com>
2026-02-05 17:59:57 -0500

    x

--- doom.d/config.el
+++ doom.d/config.el
@@ -334,7 +334,9 @@
                 (copy-marker (treesit-node-end node))))
 
         (let ((node (treesit-node-at (point) 'tsx)))
-          (while (not (string= (treesit-node-type node) "jsx_element"))
+
+          (while (not (member (treesit-node-type node)
+                              '("jsx_self_closing_element" "jsx_element")))
             (setq node (treesit-node-parent node)))
 
           (let* ((jsx-region (++treesit-node-region node))
@@ -356,7 +358,8 @@
                 (copy-marker (treesit-node-end node))))
 
         (let ((node (treesit-node-at (point) 'tsx)))
-          (while (not (string= (treesit-node-type node) "jsx_element"))
+          (while (not (member (treesit-node-type node)
+                              '("jsx_self_closing_element" "jsx_element")))
             (setq node (treesit-node-parent node)))
 
           (let* ((jsx-region (++treesit-node-region node)))
@@ -375,33 +378,49 @@
                 (copy-marker (treesit-node-end node))))
 
         (let ((node (treesit-node-at (point) 'tsx)))
-          (while (not (string= (treesit-node-type node) "jsx_element"))
+          (while (not (member (treesit-node-type node)
+                              '("jsx_self_closing_element" "jsx_element")))
             (setq node (treesit-node-parent node)))
 
-          (let* (
-                 (open-identifier-region (-> node
-                                             (treesit-node-child-by-field-name "open_tag")
-                                             (treesit-node-child-by-field-name "name")
-                                             (++treesit-node-region)))
-                 (close-identifier-region (-> node
-                                              (treesit-node-child-by-field-name "close_tag")
-                                              (treesit-node-child-by-field-name "name")
-                                              (++treesit-node-region)))
-                 (new-jsx-identifier (read-from-minibuffer "New JSX tag: ")))
+          (pcase (treesit-node-type node)
+            ("jsx_self_closing_element"
+             (let* ((open-identifier-region (-> node
+                                                (treesit-node-child-by-field-name "name")
+                                                (++treesit-node-region)))
+                    (new-jsx-identifier (read-from-minibuffer "New JSX tag: ")))
 
-            (cl-destructuring-bind (start . end) open-identifier-region
-              (save-excursion
-                (delete-region start end)
-                (goto-char start)
-                (insert new-jsx-identifier)))
-              
-            (cl-destructuring-bind (start . end) close-identifier-region
-              (save-excursion
-                (delete-region start end)
-                (goto-char start)
-                (insert new-jsx-identifier)))
+               (cl-destructuring-bind (start . end) open-identifier-region
+                 (save-excursion
+                   (delete-region start end)
+                   (goto-char start)
+                   (insert new-jsx-identifier)))
 
-            (call-interactively #'+format/buffer))))))
+               (call-interactively #'+format/buffer)))
+
+            ("jsx_element"
+             (let* ((open-identifier-region (-> node
+                                                (treesit-node-child-by-field-name "open_tag")
+                                                (treesit-node-child-by-field-name "name")
+                                                (++treesit-node-region)))
+                    (close-identifier-region (-> node
+                                                 (treesit-node-child-by-field-name "close_tag")
+                                                 (treesit-node-child-by-field-name "name")
+                                                 (++treesit-node-region)))
+                    (new-jsx-identifier (read-from-minibuffer "New JSX tag: ")))
+
+               (cl-destructuring-bind (start . end) open-identifier-region
+                 (save-excursion
+                   (delete-region start end)
+                   (goto-char start)
+                   (insert new-jsx-identifier)))
+               
+               (cl-destructuring-bind (start . end) close-identifier-region
+                 (save-excursion
+                   (delete-region start end)
+                   (goto-char start)
+                   (insert new-jsx-identifier)))
+
+               (call-interactively #'+format/buffer))))))))
 
 
 (use-package! pixel-scroll
